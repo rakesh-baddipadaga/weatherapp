a729c978e291e45608aed447ff8115b95adc0694
rakesh <rakeshbaddipadaga8@gmail.com>
2024-06-23 16:14:53 +0530

    first commit

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { format } from 'date-fns';
 
-// const API_KEY = 'YOUR_API_KEY';
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY
 
 export const fetchWeather = createAsyncThunk(
   'weather/fetchWeather',
   async (city, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=17ab1e59c7abef816b8123403980342d`,{
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,{
         params:{units:'metric'}
       });
       return response.data;
@@ -22,7 +22,7 @@ export const fetchHourlyForecast = createAsyncThunk(
   'weather/fetchHourlyForecast',
   async (city, { rejectWithValue }) => {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=17ab1e59c7abef816b8123403980342d`,{
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`,{
         params:{units:'metric'}
       });
       return response.data;
@@ -40,7 +40,7 @@ export const fetchWeeklyForecast = createAsyncThunk(
         params: {
           lat: lat,
           lon: lon,
-          appid: '17ab1e59c7abef816b8123403980342d', // Replace with your actual API key
+          appid: `${apiKey}`, // Replace with your actual API key
           units: 'metric' // Use metric units for Celsius
         }
       });
@@ -77,20 +77,6 @@ export const fetchWeeklyForecast = createAsyncThunk(
   }
 );
 
-// export const fetchWeeklyForecast = createAsyncThunk(
-//   'weather/fetchWeeklyForecast',
-//   async (city, { rejectWithValue }) => {
-//     try {
-//       const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&exclude=current,minutely,hourly,alerts&appid=17ab1e59c7abef816b8123403980342d`);
-//       return response.data.daily;
-//     } catch (error) {
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-
-
 
 
 
@@ -113,7 +99,7 @@ export const fetchWeeklyForecast = createAsyncThunk(
 //   'weather/fetchWeather',
 //   async (city, { rejectWithValue }) => {
 //     try {
-//       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=17ab1e59c7abef816b8123403980342d`);
+//       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`);
 //       return response.data;
 //     } catch (error) {
 //       return rejectWithValue(error.message);
